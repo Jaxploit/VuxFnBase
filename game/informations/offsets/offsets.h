f037fa7e93bef2189f3f4f9a4415d2bca09ac496
Jaxploit <liasyour961@gmail.com>
2025-04-01 21:02:04 +0200

    Main upload

--- game/informations/offsets/offsets.h
+++ game/informations/offsets/offsets.h
@@ -3,15 +3,15 @@
 
 namespace OFFSETS
 {
-    uintptr_t UWorld = 0x1771C988;
-    uintptr_t GameInstance = 0x208;
+    uintptr_t UWorld = 0x16F07B88;
+    uintptr_t GameInstance = 0x200;
     uintptr_t PawnPrivate = 0x320;
     uintptr_t LocalPlayers = 0x38;
     uintptr_t PlayerController = 0x30;
     uintptr_t LocalPawn = 0x350;
     uintptr_t PlayerState = 0x2C8;
     uintptr_t RootComponent = 0x1B0;
-    uintptr_t GameState = 0x190;
+    uintptr_t GameState = 0x188;
     uintptr_t Cameramanager = 0x360;
     uintptr_t Mesh = 0x328;
     uintptr_t TeamId = 0x1259;
@@ -20,3 +20,5 @@ namespace OFFSETS
     uintptr_t RelativeLocation = 0x138;
     uintptr_t PlayerArray = 0x2C0;
 }
+
+// 01.04.2025
